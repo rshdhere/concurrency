af78a4da75c4474c0dc709f80084f588181715bf
raashed <mohammedraashed10@gmail.com>
2026-04-22 11:14:50 +0530

    added some shit

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,21 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	ch := make(chan int)
 
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go func(n int) {
-			defer wg.Done()
-			fmt.Println("task", n)
-		}(i)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		ch <- 42
+	}()
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for v := range ch {
+		fmt.Println(v)
 	}
-
-	wg.Wait()
-	fmt.Println("done")
 }
